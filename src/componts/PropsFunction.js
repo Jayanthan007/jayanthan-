cee53e65dfb0981e316cc50b3b35b7a7abf0ea66
jayanthan sanjay <727821tuad024@skct.edu.in>
2022-12-10 16:01:02 +0530

    first commit

---
+++ src/componts/PropsFunction.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PropsFunctional from './Components/2PropsFunctional';
+export default function App() {
+    return(
+        <div><PropsFunctional name="Jayanthan"/></div>
+    )
+}
---
+++ src/componts/Test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+class Hello extends React.Component
+{
+    render()
+    {
+        return(
+            <h1>Am At Hello Class Component</h1>)
+    }
+}
+
+class ClassComponent extends React.Component
+{
+    render()
+    {
+        return(
+            <>
+            <ul>
+                <li>GOODMORNING</li>
+                 <li>GOODAFTERNOON</li>
+                 <li>GOODNIGHT</li>
+                
+            </ul>
+            </>
+        )
+
+    }
+}
